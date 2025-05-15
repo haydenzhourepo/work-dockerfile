82ed8087ac1f036e3f6c7a9a1d935ff9670edc30
Hayden Zhou <hayden.zhou@sap.com>
2025-05-15 17:03:21 +0800

    fix kafka and kafkaui problem

--- kafka/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-[ -n "${DEBUG:-}" ] && set -x
-
-echo "# ============================================================================ #"
-echo "                         S t a r t i n g   K a f k a"
-echo "# ============================================================================ #"
-kafka-server-start.sh /kafka/config/server.properties
-
